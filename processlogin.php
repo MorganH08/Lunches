1e084d8ac3fb3cf4bc40bbc7a52cab993f246659
MorganH08 <hopkin.mg@oundleschool.org.uk>
2025-11-18 11:30:09 +0000

    ffgg

---
+++ processlogin.php
@@ -0,0 +1,26 @@
+<?php
+//header("location: food.php");
+print_r($_POST);
+array_map("htmlspecialchars",$_POST); //sanitises inputs so no html can be injected
+include_once("connection.php");//import equivalent!
+
+try
+{
+    $stmt=$conn->prepare("SELECT * from tblusers WHERE Usenrame=:Username");
+    $stmt->bindParam(":Username", $_POST["username"]);
+    $stmt->execute();
+    
+    if ($stmt->rowCount() == 0)
+    {
+        echo("Invalid username.");
+    }
+}
+
+catch(PDOException $e)
+{
+    echo("error: " . $e->getMessage());
+}
+
+
+
+?>
