91eb886b0b5ae1594f5e13c9fe280716fad0459d
MorganH08 <hopkin.mg@oundleschool.org.uk>
2025-11-18 11:22:11 +0000

    login page

---
+++ login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Login</title>
+</head>
+
+<body>
+    <form action="processlogin.php" method="POST">
+        Username:<input type="text" name="surname"><br>
+        Password:<input type="password" name="password"><br>
+        <input type="submit" value="Login">
+</form>
+
+</body>
+</html>
