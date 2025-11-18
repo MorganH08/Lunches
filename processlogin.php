e1e4678dc55eb9c6b4d057c4788be96bef403a60
MorganH08 <hopkin.mg@oundleschool.org.uk>
2025-11-18 11:48:59 +0000

    farg

--- processlogin.php
+++ processlogin.php
@@ -1,18 +1,27 @@
 <?php
 //header("location: food.php");
 print_r($_POST);
-array_map("htmlspecialchars",$_POST); //sanitises inputs so no html can be injected
+array_map("htmlspecialchars", $_POST);//sanitises inputs so no html can be injected
 include_once("connection.php");//import equivalent!
 
-try
-{
-    $stmt=$conn->prepare("SELECT * from tblusers WHERE Username=:Username");
+try{
+    $stmt=$conn->prepare("SELECT * from tblusers WHERE Username=:Username;");
     $stmt->bindParam(":Username", $_POST["username"]);
     $stmt->execute();
     
-    if ($stmt->rowCount() == 0)
-    {
-        echo("Invalid username.");
+    if ($stmt->rowCount() == 0) {
+        echo("Invalid username ."); 
+    }else{
+        while($row=$stmt->fetch(PDO::FETCH_ASSOC))
+        {
+            print_r($row);
+            if ($_POST["password"]==$row["Password"])
+            echo("Password ok")
+        else
+        {
+            echo("Incorrect password");
+        }
+        }
     }
 }
 
