3baa52468c633f1d2df78ad5fbb0943ee5c7ee72
MorganH08 <hopkin.mg@oundleschool.org.uk>
2025-11-23 23:35:47 +0000

    restrict access to food

--- processlogin.php
+++ processlogin.php
@@ -1,7 +1,5 @@
-
 <?php
 session_start();
-
 //header("location: food.php");
 print_r($_POST);
 array_map("htmlspecialchars", $_POST);//sanitises inputs so no html can be injected
@@ -10,8 +8,8 @@ include_once("connection.php");//import equivalent!
 try{
     $stmt=$conn->prepare("SELECT * from tblusers WHERE Username=:Username;");
     $stmt->bindParam(":Username", $_POST["username"]);
-    $stmt->execute();
-    
+    $stmt->execute(); 
+
     if ($stmt->rowCount() == 0) {
         echo("Invalid username ."); 
     }else{
@@ -21,14 +19,19 @@ try{
             print_r($row);
             if ($_POST["password"]==$row["Password"]){
                 echo("password ok");
-                $_SESSION["firstname"]=$row["Uorename"];
-                $_SESSION["loggedinuser"]=$row["Username"]
+                $_SESSION["firstname"]=$row["Forename"];
+                $_SESSION["loggedinuser"]=$row["Username"];
+                $_SESSION["role"]=$row["Role"];
                 
+                if ($row["Role"]=="pupil"){
+                    echo("Only admins can log into food.php")
+                }
+                else{
+                    echo("Logged into food.php")
+                }
             }else{
                 echo("incorrect password");
             }
-            //echo($row["Name"]." ".$row["Description"]." ".$row["Price"]);
-            echo("<br>");
         }
     }
 }
